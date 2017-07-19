cd39a8ab75056ec874fe196a8268d748867e1523
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-07-19 17:45:05 -0500

    Add initial source file wavtools.cpp

---
+++ wavtools.cpp
@@ -0,0 +1,19 @@
+/*
+ * wavtools.cpp
+ *
+ *  Created on: Jul 19, 2017
+ *      Author: david
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+	cout << "Number of input args is " << argc << endl;
+	cout << "Arg loc is " << argv << endl;
+	cout << "Arg values are " << argv[0] << "\nand "
+			<< argv[1] << endl;
+	return 0;
+}
+
+
