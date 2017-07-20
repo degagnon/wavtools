5be33a6c099fa1703dec68d2bdfd77fd1d01d2c6
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-07-20 16:51:25 -0500

    Print arbitrary number of input arguments

--- wavtools.cpp
+++ wavtools.cpp
@@ -11,8 +11,11 @@ using namespace std;
 int main(int argc, char** argv){
 	cout << "Number of input args is " << argc << endl;
 	cout << "Arg loc is " << argv << endl;
-	cout << "Arg values are " << argv[0] << "\nand "
-			<< argv[1] << endl;
+	cout << "Arg values are:" << endl;
+	for(int i = 0; i < argc; ++i){
+		cout << argv[i] << endl;
+	}
+
 
 	// Here is a simple comment to test git changes.
 
