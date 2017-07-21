898d9d1666a69e756c08031bc81d0fb8160e3fc1
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-07-21 16:33:50 -0500

    Load full header and display contents

--- wavtools.cc
+++ wavtools.cc
@@ -59,11 +59,29 @@ int main(int argc, char** argv) {
     file.read(buffer, 4);
     string subchunk1_id (buffer, 4);
     uint32_t subchunk1_size = ReadLittleEndian4ByteUint(file);
+    uint16_t audio_format = ReadLittleEndian2ByteUint(file);
+    uint16_t num_channels = ReadLittleEndian2ByteUint(file);
+    uint32_t sample_rate = ReadLittleEndian4ByteUint(file);
+    uint32_t byte_rate = ReadLittleEndian4ByteUint(file);
+    uint16_t block_align = ReadLittleEndian2ByteUint(file);
+    uint16_t bits_per_sample = ReadLittleEndian2ByteUint(file);
+    file.read(buffer, 4);
+    string subchunk2_id (buffer, 4);
+    uint32_t subchunk2_size = ReadLittleEndian4ByteUint(file);
     cout << "Chunk ID: " << chunk_id << endl
          << "Chunk Size: " << chunk_size << endl
          << "Format: " << format << endl
          << "Subchunk 1 ID: " << subchunk1_id << endl
-         << "Subchunk 1 Size: " << subchunk1_size << endl;
+         << "Subchunk 1 Size: " << subchunk1_size << endl
+         << "Audio Format: " << audio_format << endl
+         << "Number of Channels: " << num_channels << endl
+         << "Sample Rate: " << sample_rate << endl
+         << "Byte Rate: " << byte_rate << endl
+         << "Block Align: " << block_align << endl
+         << "Bits per Sample: " << bits_per_sample << endl
+         << "Subchunk 2 ID: " << subchunk2_id << endl
+         << "Subchunk 2 Size: " << subchunk2_size << endl;
+    cout << "File reading is now at position " << file.tellg() << endl;
     file.close();
     cout << "File " << argv[1] << " closed." << endl;
   } else {
