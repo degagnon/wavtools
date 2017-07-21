8c435ca17406ea1c6067c202e80c6e83f49dbe72
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-07-20 20:32:53 -0500

    Read part of file, handle memory allocation

--- wavtools.cpp
+++ wavtools.cpp
@@ -28,8 +28,15 @@ int main(int argc, char** argv){
 		cout << "File opened." << endl;
 		filesize = file.tellg();
 		cout << "File size is " << filesize << " bytes" << endl;
+		data = new char [filesize];
+		file.seekg(ios::beg);
+		file.read(data, filesize);
 		file.close();
 		cout << "File closed." << endl;
+		for(int i = 0; i < 50; ++i){
+			cout << data[i] << endl;
+		}
+		delete[] data;
 	}
 	else{
 		cout << "File was not opened.";
