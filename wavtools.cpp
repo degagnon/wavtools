23625ec7f75368b1b9d5c0d0902a6048fa6ee27f
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-07-20 20:57:55 -0500

    Modify tabs/spaces to match Google format

--- wavtools.cpp
+++ wavtools.cpp
@@ -13,36 +13,37 @@
 using namespace std;
 
 int main(int argc, char** argv){
-	cout << "Number of input args is " << argc << endl;
-	cout << "Arg loc is " << argv << endl;
-	cout << "Arg values are:" << endl;
-	for(int i = 0; i < argc; ++i){
-		cout << argv[i] << endl;
-	}
+  cout << "Number of input args is " << argc << endl;
+  cout << "Arg loc is " << argv << endl;
+  cout << "Arg values are:" << endl;
+  for(int i = 0; i < argc; ++i){
+    cout << argv[i] << endl;
+  }
 
-	char* data;
-	streampos filesize;
+  char* data;
+  streampos filesize;
 
-	ifstream file (argv[1], ios::in|ios::binary|ios::ate);
-	if(file.is_open()){
-		cout << "File " << argv[1] << " opened." << endl;
-		filesize = file.tellg();
-		cout << "File size is " << filesize << " bytes" << endl;
-		data = new char [filesize];
-		file.seekg(ios::beg);
-		file.read(data, filesize);
-		file.close();
-		cout << "File " << argv[1] << " closed." << endl;
-		for(int i = 0; i < 50; ++i){
-			cout << data[i] << endl;
-		}
-		delete[] data;
-	}
-	else{
-		cout << "File was not opened.";
-	}
+  ifstream file (argv[1], ios::in|ios::binary|ios::ate);
+  if(file.is_open()){
+    cout << "File " << argv[1] << " opened." << endl;
+    filesize = file.tellg();
+    cout << "File size is " << filesize << " bytes" << endl;
+    data = new char [filesize];
+    file.seekg(ios::beg);
+    file.read(data, filesize);
+    file.close();
+    cout << "File " << argv[1] << " closed." << endl;
+    for(int i = 0; i < 50; ++i){
+      cout << data[i];
+    }
+    cout << endl;
+    delete[] data;
+  }
+  else{
+    cout << "File was not opened.";
+  }
 
-	return 0;
+  return 0;
 }
 
 
