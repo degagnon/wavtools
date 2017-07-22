c0cb40f0083f1b5e67d15c1c2eea17c42533f32c
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-07-22 17:20:35 -0500

    Replace individual header pieces with structs for file reading

--- wavtools.cc
+++ wavtools.cc
@@ -23,7 +23,7 @@ struct RiffHeader {
 };
 
 struct FmtHeader {
-  char subchunk_id[4];
+  char subchunk1_id[4];
   uint32_t subchunk1_size;
   uint16_t audio_format;
   uint16_t num_channels;
@@ -36,25 +36,10 @@ struct FmtHeader {
 // TODO(David): Add flexibility to handle extended formats with extra bytes.
 
 struct DataHeader {
-  char subchunk_id[4];
-  uint32_t subchunk1_size;
+  char subchunk2_id[4];
+  uint32_t subchunk2_size;
   // Data size will vary by file and therefore is handled separately.
 };
-
-
-uint16_t ReadLittleEndian2ByteUint(ifstream& filestream) {
-  uint16_t value;
-  cout << "Value is size " << sizeof(value) << endl;
-  filestream.read(reinterpret_cast<char*>(&value), sizeof(value));
-  return value;
-}
-
-uint32_t ReadLittleEndian4ByteUint(ifstream& filestream) {
-  uint32_t value;
-  cout << "Value is size " << sizeof(value) << endl;
-  filestream.read(reinterpret_cast<char*>(&value), sizeof(value));
-  return value;
-}
 
 int main(int argc, char** argv) {
   cout << "Number of input args is " << argc << endl;
@@ -64,7 +49,6 @@ int main(int argc, char** argv) {
     cout << argv[i] << endl;
   }
 
-//  char* data;
   streampos filesize;
   ifstream file (argv[1], ios::in|ios::binary|ios::ate);
   if (file.is_open()) {
@@ -72,38 +56,26 @@ int main(int argc, char** argv) {
     filesize = file.tellg();
     cout << "File size is " << filesize << " bytes" << endl;
 
+    RiffHeader riff_info;
+    FmtHeader format_info;
+    DataHeader data_info;
     file.seekg(ios::beg);
-    char buffer[4];
-    file.read(buffer, 4);
-    string chunk_id (buffer, 4);
-    uint32_t chunk_size = ReadLittleEndian4ByteUint(file);
-    file.read(buffer, 4);
-    string format (buffer, 4);
-    file.read(buffer, 4);
-    string subchunk1_id (buffer, 4);
-    uint32_t subchunk1_size = ReadLittleEndian4ByteUint(file);
-    uint16_t audio_format = ReadLittleEndian2ByteUint(file);
-    uint16_t num_channels = ReadLittleEndian2ByteUint(file);
-    uint32_t sample_rate = ReadLittleEndian4ByteUint(file);
-    uint32_t byte_rate = ReadLittleEndian4ByteUint(file);
-    uint16_t block_align = ReadLittleEndian2ByteUint(file);
-    uint16_t bits_per_sample = ReadLittleEndian2ByteUint(file);
-    file.read(buffer, 4);
-    string subchunk2_id (buffer, 4);
-    uint32_t subchunk2_size = ReadLittleEndian4ByteUint(file);
-    cout << "Chunk ID: " << chunk_id << endl
-         << "Chunk Size: " << chunk_size << endl
-         << "Format: " << format << endl
-         << "Subchunk 1 ID: " << subchunk1_id << endl
-         << "Subchunk 1 Size: " << subchunk1_size << endl
-         << "Audio Format: " << audio_format << endl
-         << "Number of Channels: " << num_channels << endl
-         << "Sample Rate: " << sample_rate << endl
-         << "Byte Rate: " << byte_rate << endl
-         << "Block Align: " << block_align << endl
-         << "Bits per Sample: " << bits_per_sample << endl
-         << "Subchunk 2 ID: " << subchunk2_id << endl
-         << "Subchunk 2 Size: " << subchunk2_size << endl;
+    file.read(reinterpret_cast<char*>(&riff_info), sizeof(riff_info));
+    file.read(reinterpret_cast<char*>(&format_info), sizeof(format_info));
+    file.read(reinterpret_cast<char*>(&data_info), sizeof(data_info));
+    cout << "Chunk ID: " << riff_info.chunk_id << endl
+         << "Chunk Size: " << riff_info.chunk_size << endl
+         << "Format: " << riff_info.format << endl
+         << "Subchunk 1 ID: " << format_info.subchunk1_id << endl
+         << "Subchunk 1 Size: " << format_info.subchunk1_size << endl
+         << "Audio Format: " << format_info.audio_format << endl
+         << "Number of Channels: " << format_info.num_channels << endl
+         << "Sample Rate: " << format_info.sample_rate << endl
+         << "Byte Rate: " << format_info.byte_rate << endl
+         << "Block Align: " << format_info.block_align << endl
+         << "Bits per Sample: " << format_info.bits_per_sample << endl
+         << "Subchunk 2 ID: " << data_info.subchunk2_id << endl
+         << "Subchunk 2 Size: " << data_info.subchunk2_size << endl;
     cout << "File reading is now at position " << file.tellg() << endl;
     file.close();
     cout << "File " << argv[1] << " closed." << endl;
