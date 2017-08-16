f738164c755e0c4807ce5c15d091d47298f56a44
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-08-15 23:02:31 -0500

    Refactor WavFile class for modular chunk handling and reading float data

--- wavtools.cc
+++ wavtools.cc
@@ -24,20 +24,16 @@ struct ChunkHeader {
   uint32_t size;
 };
 
-inline void PrintFourChars (const char* label) {
+inline void PrintFourChars(const char* label) {
   int kLabelSize = 4;
   std::cout.write(label, kLabelSize);
 }
 
-struct RiffHeader {
-  char chunk_id[4];
-  uint32_t chunk_size;
+struct RiffContents {
   char format[4];
 };
 
-struct FmtHeader {
-  char subchunk_id[4];
-  uint32_t subchunk_size;
+struct FmtContents {
   uint16_t audio_format;
   uint16_t num_channels;
   uint32_t sample_rate;
@@ -45,17 +41,8 @@ struct FmtHeader {
   uint16_t block_align;
   uint16_t bits_per_sample;
 };
-// TODO(David): Handle extended formats with extra bytes (low priority).
 
-struct DataHeader {
-  char subchunk_id[4];
-  uint32_t subchunk_size;
-  // Data size could vary by file and therefore is handled separately.
-};
-
-struct FactHeader {
-  char subchunk_id[4];
-  uint32_t subchunk_size;
+struct FactContents {
   uint32_t num_samples;
 };
 
@@ -70,7 +57,7 @@ class Signal {
   double GetTimeScalePoint(int index) {return time_scale_[index];};
   int GetSampleRate() {return sample_rate_;};
   int GetNumSamples() {return num_samples_;};
-
+  
  private:
   std::vector<T> waveform_;
   std::vector<double> time_scale_;
@@ -94,21 +81,26 @@ class WavFile {
   void PrintInfo();
   void PrintAllInfo();
   void PrintHead(int);
-  int GetNumChannels() {return format_header_.num_channels;};
+  void PrintChunks();
+  int GetNumChannels() {return format_.num_channels;};
   int GetNumSamples() {return num_samples_;};
-  int GetSampleRate() {return format_header_.sample_rate;};
+  int GetSampleRate() {return format_.sample_rate;};
   Signal<int16_t> ExtractSignal(int);
 
  private:
   std::string filename_;
   std::streampos filesize_;
-  RiffHeader riff_header_;
-  FmtHeader format_header_;
-  FactHeader fact_header_;
-  DataHeader data_header_;
+  RiffContents riff_;
+  FmtContents format_;
+  FactContents fact_;
   int num_samples_;
-  std::vector<std::vector<int16_t> > data_;
-  std::vector<char> remaining_chunks_;
+  std::vector<std::vector<int16_t> > data_int_;
+  std::vector<std::vector<float> > data_float_;
+  std::vector<std::string> chunk_ids_;
+  std::vector<int32_t> chunk_sizes_;
+  std::vector<std::vector<char> > other_data_;
+  int kLabelSize_ = 4;
+
 };
 WavFile::WavFile(std::string filename_input) {
   // The constructor handles file access and data loading, which might be a
@@ -116,33 +108,66 @@ WavFile::WavFile(std::string filename_input) {
   // object are tightly tied to the actual file, and using a separate ReadWav()
   // routine could create initialization issues. Possibly worth revisiting.
   filename_ = filename_input;
-  std::ifstream file (filename_, std::ios::in|std::ios::binary|std::ios::ate);
+  std::ifstream file(filename_, std::ios::in | std::ios::binary | std::ios::ate);
   if (file.is_open()) {
     std::cout << "File " << filename_ << " opened." << std::endl;
     filesize_ = file.tellg();
     file.seekg(std::ios::beg);
-    file.read(reinterpret_cast<char*>(&riff_header_), sizeof(riff_header_));
-    file.read(reinterpret_cast<char*>(&format_header_), sizeof(format_header_));
-    if (format_header_.audio_format != 1) {
-      // Non-PCM formats require the Fact chunk.
-      file.read(reinterpret_cast<char*>(&fact_header_), sizeof(fact_header_));
-    }
-    file.read(reinterpret_cast<char*>(&data_header_), sizeof(data_header_));
-    // The data vector is structured such that we load the values for each
-    // channel into a separate sub-vector. Element access is [channel][sample].
-    // Within the class, the data 2D vector needs resizing to fit the data.
-    num_samples_ = data_header_.subchunk_size / format_header_.block_align;
-    data_.resize(format_header_.num_channels);
-    for (int i = 0; i < format_header_.num_channels; ++i) {
-      data_[i].resize(num_samples_);
-    }
-    for (int j = 0; j < num_samples_; ++j) {
-      for (int i = 0; i < format_header_.num_channels; ++i) {
-        file.read(reinterpret_cast<char*>(&data_[i][j]), sizeof(data_[i][j]));
+    while (file.tellg() < filesize_) {
+      ChunkHeader new_header;
+      file.read(reinterpret_cast<char*>(&new_header), sizeof (new_header));
+      chunk_ids_.push_back(std::string(new_header.id, kLabelSize_));
+      chunk_sizes_.push_back(new_header.size);
+      if (chunk_ids_.back().compare("RIFF") == 0) {
+        file.read(reinterpret_cast<char*>(&riff_), kLabelSize_);
+      } else if (chunk_ids_.back().compare("fmt ") == 0) {
+        file.read(reinterpret_cast<char*>(&format_), sizeof (format_));
+        if (format_.audio_format == 1 || format_.audio_format == 3) {
+          continue;
+        } else {
+          std::cout << "This program can only handle 16-bit PCM (fmt code 1)\n"
+              << "or 32-bit IEEE float (fmt code 3).\n"
+              << "Audio data not readable.";
+          break;
+        }
+      } else if (chunk_ids_.back().compare("fact") == 0) {
+        file.read(reinterpret_cast<char*>(&fact_), sizeof (fact_));
+        num_samples_ = fact_.num_samples;
+      } else if (chunk_ids_.back().compare("PEAK") == 0) {
+        std::vector<char> char_buffer(chunk_sizes_.back(), '0');
+        file.read(&char_buffer[0], char_buffer.size());
+        other_data_.push_back(char_buffer);
+      } else if (chunk_ids_.back().compare("data") == 0) {
+        // Data vector element access is [channel][sample].
+        // The vector needs resizing before elements can be accessed.
+        if (format_.audio_format == 1) {
+          num_samples_ = chunk_sizes_.back() / format_.block_align;
+          data_int_.resize(format_.num_channels);
+          for (int i = 0; i < format_.num_channels; ++i) {
+            data_int_[i].resize(num_samples_);
+          }
+          for (int j = 0; j < num_samples_; ++j) {
+            for (int i = 0; i < format_.num_channels; ++i) {
+              file.read(reinterpret_cast<char*>(&data_int_[i][j]), sizeof(data_int_[i][j]));
+            }
+          }
+        } else if (format_.audio_format == 3) {
+          data_float_.resize(format_.num_channels);
+          for (int i = 0; i < format_.num_channels; ++i) {
+            data_float_[i].resize(num_samples_);
+          }
+          for (int j = 0; j < num_samples_; ++j) {
+            for (int i = 0; i < format_.num_channels; ++i) {
+              file.read(reinterpret_cast<char*>(&data_float_[i][j]), sizeof(data_float_[i][j]));
+            }
+          }
+        }
+      } else {
+        std::vector<char> char_buffer(chunk_sizes_.back(), '0');
+        file.read(&char_buffer[0], char_buffer.size());
+        other_data_.push_back(char_buffer);
       }
     }
-    std::vector<char> remaining_chunks(filesize_ - file.tellg(), '0');
-    file.read(&remaining_chunks[0], remaining_chunks.size());
     file.close();
     std::cout << "File " << filename_ << " closed." << std::endl;
   } else {
@@ -150,48 +175,34 @@ WavFile::WavFile(std::string filename_input) {
   }
 }
 void WavFile::PrintInfo() {
-  std::cout << "Data is organized into " << format_header_.num_channels
+  std::cout << "Data is organized into " << format_.num_channels
       << " channels, each with " << num_samples_ << " samples.\n"
-      << "Sample rate = " << format_header_.sample_rate
+      << "Sample rate = " << format_.sample_rate
       << " samples per second.\n"
-      << "Block Align = " << format_header_.block_align
+      << "Block Align = " << format_.block_align
       << " bytes per sample, including all channels.\n"
-      << "Data point size: " << format_header_.bits_per_sample
+      << "Data point size: " << format_.bits_per_sample
       << " bits per sample, single channel." << std::endl;
 }
 void WavFile::PrintAllInfo() {
-  std::cout << "*** Riff Header ***\nChunk ID: ";
-  PrintFourChars(riff_header_.chunk_id);
-  std::cout << "\nChunk size: " << riff_header_.chunk_size << '\n';
-  std::cout << "Format: ";
-  PrintFourChars(riff_header_.format);
-  std::cout << "\n*** Format Header ***\nSubchunk ID: ";
-  PrintFourChars(format_header_.subchunk_id);
-  std::cout << "\nSize: " << format_header_.subchunk_size << '\n';
-  std::cout << "Format: " << format_header_.audio_format << '\n';
-  std::cout << "Number of Channels: " << format_header_.num_channels << '\n';
-  std::cout << "Sample Rate: " << format_header_.sample_rate << '\n';
-  std::cout << "Byte Rate: " << format_header_.byte_rate << '\n';
-  std::cout << "Block Align: " << format_header_.block_align << '\n';
-  std::cout << "Bits per Sample: " << format_header_.bits_per_sample << '\n';
-  if (format_header_.audio_format != 1) {
-    std::cout << "*** Fact Header ***\nSubchunk ID: ";
-    PrintFourChars(fact_header_.subchunk_id);
-    std::cout << "\nSize: " << fact_header_.subchunk_size << '\n';
-    std::cout << "Number of Samples: " << fact_header_.num_samples << '\n';
-  }
-  std::cout << "*** Data Header ***\nSubchunk ID: ";
-  PrintFourChars(data_header_.subchunk_id);
-  std::cout << "\nSize: " << data_header_.subchunk_size << '\n';
+  std::cout << "Riff Type: \n";
+  PrintFourChars(riff_.format);
+  std::cout << "\nFormat: " << format_.audio_format << '\n';
+  std::cout << "Number of Channels: " << format_.num_channels << '\n';
+  std::cout << "Sample Rate: " << format_.sample_rate << '\n';
+  std::cout << "Byte Rate: " << format_.byte_rate << '\n';
+  std::cout << "Block Align: " << format_.block_align << '\n';
+  std::cout << "Bits per Sample: " << format_.bits_per_sample << '\n';
+  std::cout << "Number of Samples: " << num_samples_ << '\n';
 }
 void WavFile::PrintHead(int segment_length) {
   if (segment_length > 0 && segment_length < num_samples_) {
-    for (int i = 0; i < format_header_.num_channels; ++i) {
+    for (int i = 0; i < format_.num_channels; ++i) {
       std::cout << "Channel " << i << ": ";
       // Ternary operator helps avoid accessing nonexistent data
-      for (int j = 0; j < ((segment_length < data_[i].size()) ?
-                           segment_length : data_[i].size()); ++j) {
-         std::cout << data_[i][j] << " ";
+      for (int j = 0; j < ((segment_length < data_int_[i].size()) ?
+                           segment_length : data_int_[i].size()); ++j) {
+        std::cout << data_int_[i][j] << " ";
       }
       std::cout << '\n';
     }
@@ -199,21 +210,30 @@ void WavFile::PrintHead(int segment_length) {
     std::cout << segment_length << " is not a valid length." << std::endl;
   }
 }
+void WavFile::PrintChunks() {
+  std::cout << "Chunk Names | Chunk Sizes (Bytes)\n";
+  for (int i = 0; i < chunk_ids_.size(); ++i) {
+    std::cout << "       " << chunk_ids_[i] << " | " << chunk_sizes_[i] << '\n';
+  }
+  std::cout << std::endl;
+}
 Signal<int16_t> WavFile::ExtractSignal(int selected_channel) {
   std::vector<int16_t> contents;
-  if (selected_channel >= 0 && selected_channel < format_header_.num_channels) {
-    contents = std::move(data_[selected_channel]);
+  if (selected_channel >= 0 && selected_channel < format_.num_channels) {
+    contents = std::move(data_int_[selected_channel]);
   } else {
     std::cout << "Invalid channel. Empty signal returned.";
   }
-  Signal<int16_t> exported_signal (contents, format_header_.sample_rate);
+  Signal<int16_t> exported_signal(contents, format_.sample_rate);
   return exported_signal;
 }
 
-class Plotter{
+class Plotter {
   // Governs interactions with gnuplot, including plot settings
  public:
-  Plotter() {};
+
+  Plotter() {
+  };
   void AddSignal(const Signal<int16_t>& signal_input);
   void Plot();
 
@@ -266,9 +286,9 @@ void Plotter::Plot() {
       delimiter = "\"";
     }
     system_command += "'" + file_to_write_ + "' using " +
-                      std::to_string(i*2+1) + ":" + std::to_string(i*2 + 2) +
-                      " title 'Channel " + std::to_string(i + 1) +
-                      "' with lines" + delimiter;
+        std::to_string(i * 2 + 1) + ":" + std::to_string(i * 2 + 2) +
+        " title 'Channel " + std::to_string(i + 1) +
+        "' with lines" + delimiter;
   }
   std::cout << "Plotting instruction: \n" << system_command << std::endl;
   // The system() command is expedient here for calling gnuplot.
@@ -277,7 +297,7 @@ void Plotter::Plot() {
   system(system_command.c_str());
 }
 
-}  // namespace wav_names
+} // namespace wav_names
 
 int main(int argc, char** argv) {
   std::cout << "Number of input args is " << argc << std::endl;
@@ -287,18 +307,19 @@ int main(int argc, char** argv) {
     std::cout << argv[i] << std::endl;
   }
 
-  wav::WavFile wav_file (argv[1]);
+  wav::WavFile wav_file(argv[1]);
   wav_file.PrintInfo();
   wav_file.PrintAllInfo();
-//  wav_file.PrintHead(10);
-//  wav::Plotter plot;
-//  for (int i = 0; i < wav_file.GetNumChannels(); ++i) {
-//    wav::Signal<int16_t> temporary_signal = wav_file.ExtractSignal(i);
-//    plot.AddSignal(temporary_signal);
-//  }
-//  plot.Plot();
-//  // Test whether signals have been fully extracted
-//  wav_file.PrintHead(10);
+  wav_file.PrintChunks();
+  //  wav_file.PrintHead(10);
+  //  wav::Plotter plot;
+  //  for (int i = 0; i < wav_file.GetNumChannels(); ++i) {
+  //    wav::Signal<int16_t> temporary_signal = wav_file.ExtractSignal(i);
+  //    plot.AddSignal(temporary_signal);
+  //  }
+  //  plot.Plot();
+  //  // Test whether signals have been fully extracted
+  //  wav_file.PrintHead(10);
 
   return 0;
 }
