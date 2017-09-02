2860b2713714bc6b639a881ce8cfd51aa36f8cac
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-09-02 17:20:11 -0500

    Move main() code into main.cc file

---
+++ main.cc
@@ -0,0 +1,22 @@
+// main file for using wavtools library
+
+#include <wavtools.h>
+
+int main(int argc, char** argv) {
+    wav::FileLoader file_raw(argv[1]);
+    file_raw.PrintChunks();
+    wav::FileParser file_parse(file_raw);
+    file_parse.PrintAllInfo();
+    std::vector<wav::Series<double>> waveforms = file_parse.ExtractChannels();
+    wav::Series<double> time_axis(
+        waveforms[0].CreateTimeScale(file_parse.GetSampleRate()));
+    wav::Plotter<double> plot;
+    for (size_t i = 0; i < waveforms.size(); ++i) {
+      std::cout << "Channel " << i << ": ";
+      waveforms[i].PrintHead(10);
+      plot.AddSeriesPair(time_axis, waveforms[i]);
+    }
+    plot.Plot();
+  
+    return 0;
+  }
--- wavtools.cc
+++ wavtools.cc
@@ -10,6 +10,9 @@
 // which is adequate for local usage,
 // but requires modification for porting to big-endian systems.
 
+#ifndef <WAVTOOLS_H>
+#define <WAVTOOLS_H>
+
 #include <algorithm>  // find()
 #include <cstdint>    // exact integer sizes used to interpret wav file format
 #include <fstream>
@@ -337,21 +340,4 @@ void Plotter<T>::Plot() {
 
 }  // namespace wav_names
 
-int main(int argc, char** argv) {
-  wav::FileLoader file_raw(argv[1]);
-  file_raw.PrintChunks();
-  wav::FileParser file_parse(file_raw);
-  file_parse.PrintAllInfo();
-  std::vector<wav::Series<double>> waveforms = file_parse.ExtractChannels();
-  wav::Series<double> time_axis(
-      waveforms[0].CreateTimeScale(file_parse.GetSampleRate()));
-  wav::Plotter<double> plot;
-  for (size_t i = 0; i < waveforms.size(); ++i) {
-    std::cout << "Channel " << i << ": ";
-    waveforms[i].PrintHead(10);
-    plot.AddSeriesPair(time_axis, waveforms[i]);
-  }
-  plot.Plot();
 
-  return 0;
-}
