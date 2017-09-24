beb24389fab1a3d138ba3944db826c899ba60ac6
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-09-24 12:53:31 -0500

    Handle cmd line input with more or less than one argument

--- main.cc
+++ main.cc
@@ -3,8 +3,17 @@
 #include "wavtools.h"
 
 int main(int argc, char** argv) {
-  // TODO(David): Handle crash when input argument does not exist
-  wav::FileLoader file_raw(argv[1]);
+  std::string file_name;
+  if (argc == 1) {
+    // No arguments given. Assign dummy name (for now)
+    file_name = "triaudio.wav";
+  } else {
+    file_name = argv[1];
+    if (argc > 2) {
+      std::cout << "Extra arguments ignored.\n";
+    }
+  }
+  wav::FileLoader file_raw(file_name);
   // TODO(David): Allow user to re-enter filename when not found
   file_raw.PrintChunks();
   wav::FileParser file_parse(file_raw);
