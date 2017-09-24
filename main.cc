b653f4fcbd9599e1fe8b0eceff1c4308f034d125
David Gagnon <23271165+degagnon@users.noreply.github.com>
2017-09-23 22:15:38 -0500

    Format indents, add TODO comments

--- main.cc
+++ main.cc
@@ -3,20 +3,22 @@
 #include "wavtools.h"
 
 int main(int argc, char** argv) {
-    wav::FileLoader file_raw(argv[1]);
-    file_raw.PrintChunks();
-    wav::FileParser file_parse(file_raw);
-    file_parse.PrintAllInfo();
-    std::vector<wav::Series<double>> waveforms = file_parse.ExtractChannels();
-    wav::Series<double> time_axis(
-        waveforms[0].CreateTimeScale(file_parse.GetSampleRate()));
-    wav::Plotter<double> plot;
-    for (size_t i = 0; i < waveforms.size(); ++i) {
-      std::cout << "Channel " << i << ": ";
-      waveforms[i].PrintHead(10);
-      plot.AddSeriesPair(time_axis, waveforms[i]);
-    }
-    plot.Plot();
-  
-    return 0;
+  // TODO(David): Handle crash when input argument does not exist
+  wav::FileLoader file_raw(argv[1]);
+  // TODO(David): Allow user to re-enter filename when not found
+  file_raw.PrintChunks();
+  wav::FileParser file_parse(file_raw);
+  file_parse.PrintAllInfo();
+  std::vector<wav::Series<double>> waveforms = file_parse.ExtractChannels();
+  wav::Series<double> time_axis(
+      waveforms[0].CreateTimeScale(file_parse.GetSampleRate()));
+  wav::Plotter<double> plot;
+  for (size_t i = 0; i < waveforms.size(); ++i) {
+    std::cout << "Channel " << i << ": ";
+    waveforms[i].PrintHead(10);
+    plot.AddSeriesPair(time_axis, waveforms[i]);
   }
+  plot.Plot();
+
+  return 0;
+}
